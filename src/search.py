87ae49aaf7d00edd2e8ad3ab12a40d1234410a9b
Robert McKnight <robby.mcknight@gmail.com>
2023-11-08 08:07:01 -0500

    Attempt to add activity filtering

--- src/search.py
+++ src/search.py
@@ -9,7 +9,12 @@ from loguru import logger
 from line_counting import get_lines_in_repo
 
 GH_TOKEN = os.getenv("GH_TOKEN")
-logger.debug(GH_TOKEN)
+
+class RepoData:
+    name: str
+    contributors: int
+    stars: int
+    lines: int
 
 # Params
 language = "python"
@@ -17,6 +22,7 @@ license_type = "mit"
 size_lower_bound = 1e3 # 1MB
 size_upper_bound = 20e3 # 20MB
 stars_lower_bound = 40000
+active_weeks_cutoff = 4
 contributors_lower_bound = 30
 num_lines_lower_bound = 8e3
 num_lines_upper_bound = 30e3
@@ -52,6 +58,7 @@ def clone_repo(repo_name):
     Git().clone(clone_url, clone_path)
     return clone_path
 
+logger.info(f"Filtering on contributors (>{contributors_lower_bound})")
 # Filter on number of contributors
 enough_contributor_repos = []
 for repo in filtered_repos:
@@ -61,17 +68,25 @@ for repo in filtered_repos:
 filtered_repos = enough_contributor_repos
 logger.info(f"Remaining repos: {len(filtered_repos)}")
 
+logger.info(f"Filtering on activity (active in last {active_weeks_cutoff})")
 # Filter on activity
 active_repos = []
 for repo in filtered_repos:
     stats = repo.get_stats_commit_activity()
-    breakpoint()
+    total_commits_last_month = sum([s.total for s in stats[-1*active_weeks_cutoff:]])
 
+    # Add if active in the last month
+    if total_commits_last_month > 0:
+        active_repos.append(repo)
+
+filtered_repos = active_repos
+logger.info(f"Remaining repos: {len(filtered_repos)}")
+
+logger.info(f"Filtering on lines (from {num_lines_lower_bound} to {num_lines_upper_bound})")
 # Filter on the number of lines
 repos_with_right_lines = []
 for repo in filtered_repos:
     logger.info(repo.full_name)
-    logger.info(f"Num contributors: {repo.get_contributors().totalCount}")
 
     # clone the repo
     repo_path = clone_repo(repo.full_name)
@@ -79,9 +94,12 @@ for repo in filtered_repos:
     if num_lines > num_lines_lower_bound and num_lines < num_lines_upper_bound:
         repos_with_right_lines.append(repo)
 filtered_repos = repos_with_right_lines
+logger.info(f"Remaining repos: {len(filtered_repos)}")
 
 
 
 logger.info(f"Remaining repos: {len(filtered_repos)}")
 for repo in filtered_repos:
-    logger.info(repo.full_name)
+    logger.info(f"Name: {repo.full_name}")
+    logger.info(f"Contributors: {repo.get_contributors().totalCount}")
+    logger.info(f"Stars: {repo.stargazers_count}")
